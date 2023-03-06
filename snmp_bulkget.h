5aa4840e12e3bd002ba912808ef3663bad4791ae
Lorenz Kästle <lorenz.kaestle@netways.de>
2023-03-06 14:11:57 +0100

    Change toolchain
    This commit puts the main function in a separate check_interfaces.c
    to make it more obvious what happens.
    The code is now (badly) separated from snmp_bulkget.c and needs further
    logical separation
    
    configure.ac is enhanced with this commit and the Makefile generation
    is now done from a Makefile.am, so the Makefile.in in this repository
    is theoretically obsolete.
    
    Some other (for now empty) files in here are just generated to satisfy
    the needs of the autoconf toolchain

--- snmp_bulkget.h
+++ snmp_bulkget.h
@@ -1,6 +1,7 @@
 
-
 #include <limits.h>
+
+#include <stdbool.h>
 
 #ifdef HAVE_GETADDRINFO
 #include <netdb.h>
@@ -69,84 +70,10 @@ struct OIDStruct {
 	size_t name_len;
 };
 
-/*
- * text strings to output in the perfdata
- */
-
-static char *if_vars_default[] = {"inOctets",	 "outOctets", "inDiscards",
-								  "outDiscards", "inErrors",  "outErrors",
-								  "inUcast",	 "outUcast",  "speed",
-								  "inBitps",	 "outBitps"};
-
-static char *if_vars_cisco[] = {"inOctets",	   "outOctets", "inDiscards",
-								"outDiscards", "inCRCs",	"outCollisions",
-								"inUcast",	   "outUcast",	"speed",
-								"inBitps",	   "outBitps"};
-
-/*
- * OIDs, hardcoded to remove the dependency on MIBs
- */
-static char *oid_if_bulkget[] = {".1.3.6.1.2.1.1.3", ".1.3.6.1.2.1.2.1",
-								 ".1.3.6.1.2.1.2.2.1.2",
-								 0}; /* "uptime", "ifNumber", "ifDescr" */
-static char *oid_if_get[] = {".1.3.6.1.2.1.1.3.0", ".1.3.6.1.2.1.2.1.0",
-							 ".1.3.6.1.2.1.2.2.1.2.1",
-							 0}; /* "uptime", "ifNumber", "ifDescr" */
-static char *oid_if_bintec[] = {".1.3.6.1.2.1.1.3.0", ".1.3.6.1.2.1.2.1.0",
-								".1.3.6.1.2.1.2.2.1.2.0",
-								0}; /* "uptime", "ifNumber", "ifDescr" */
-static char *oid_alias_bulkget[] = {".1.3.6.1.2.1.31.1.1.1.18",
-									0};							  /* "alias" */
-static char *oid_alias_get[] = {".1.3.6.1.2.1.31.1.1.1.18.1", 0}; /* "alias" */
-static char *oid_alias_bintec[] = {".1.3.6.1.2.1.31.1.1.1.18.0",
-								   0};							   /* "alias" */
-static char *oid_names_bulkget[] = {".1.3.6.1.2.1.31.1.1.1.1", 0}; /* "name" */
-static char *oid_names_get[] = {".1.3.6.1.2.1.31.1.1.1.1.1", 0};   /* "name" */
-static char *oid_names_bintec[] = {".1.3.6.1.2.1.31.1.1.1.1.0",
-								   0}; /* "name - NOT TESTED!" */
-
-static char *oid_vals_default[] = {".1.3.6.1.2.1.2.2.1.7",	/* ifAdminStatus */
-								   ".1.3.6.1.2.1.2.2.1.8",	/* ifOperStatus */
-								   ".1.3.6.1.2.1.2.2.1.10", /* ifInOctets */
-								   ".1.3.6.1.2.1.2.2.1.13", /* ifInDiscards */
-								   ".1.3.6.1.2.1.2.2.1.14", /* ifInErrors */
-								   ".1.3.6.1.2.1.2.2.1.16", /* ifOutOctets */
-								   ".1.3.6.1.2.1.2.2.1.19", /* ifOutDiscards */
-								   ".1.3.6.1.2.1.2.2.1.20", /* ifOutErrors */
-								   0};
-
-static char *oid_vals_cisco[] = {
-	".1.3.6.1.2.1.2.2.1.7",		 /* ifAdminStatus */
-	".1.3.6.1.2.1.2.2.1.8",		 /* ifOperStatus */
-	".1.3.6.1.2.1.2.2.1.10",	 /* ifInOctets */
-	".1.3.6.1.2.1.2.2.1.13",	 /* ifInDiscards */
-	".1.3.6.1.4.1.9.2.2.1.1.12", /* locIfInCRC */
-	".1.3.6.1.2.1.2.2.1.16",	 /* ifOutOctets */
-	".1.3.6.1.2.1.2.2.1.19",	 /* ifOutDiscards */
-	".1.3.6.1.4.1.9.2.2.1.1.25", /* locIfCollisions */
-	0};
-
-static char *oid_extended[] = {".1.3.6.1.2.1.31.1.1.1.6",  /* ifHCInOctets */
-							   ".1.3.6.1.2.1.31.1.1.1.10", /* ifHCOutOctets */
-							   ".1.3.6.1.2.1.2.2.1.11",	   /* ifInUcastPkts */
-							   ".1.3.6.1.2.1.2.2.1.17",	   /* ifOutUcastPkts */
-							   ".1.3.6.1.2.1.2.2.1.5",	   /* ifSpeed */
-							   ".1.3.6.1.2.1.31.1.1.1.15", /* ifHighSpeed */
-							   ".1.3.6.1.2.1.31.1.1.1.18", /* alias */
-							   ".1.3.6.1.2.1.31.1.1.1.1",  /* name */
-							   0};
-
-static char *oid_extended_cisco[] = {
-	".1.3.6.1.4.1.9.5.1.4.1.1.23", /* portAdditionalOperStatus */
-	0};
-
-static char default_community[] = "public";
 
 /*
  * operating modes
  */
-
-const char *modes[] = {"default", "cisco", "nonbulk", "bintec", NULL};
 enum mode_enum { DEFAULT, CISCO, NONBULK, BINTEC };
 
 // Config
@@ -168,7 +95,7 @@ typedef struct configuration_struct {
 	char *auth_pass;
 	char *priv_proto;
 	char *priv_pass;
-	int trimdescr;
+	unsigned int trimdescr;
 	enum mode_enum mode; // hardware mode
 	char *prefix;
 	char *list;
@@ -186,6 +113,9 @@ typedef struct configuration_struct {
 /*
  * prototypes
  */
+size_t sizeof_oid_if_bintec(void);
+size_t sizeof_oid_if_get(void);
+size_t sizeof_oid_if_bulkget(void);
 
 void print64(struct counter64 *, unsigned long *);
 u64 convertto64(struct counter64 *, unsigned long *);
@@ -204,3 +134,4 @@ void set_value(struct ifStruct *, char *, char *, u64, enum mode_enum);
 int parseoids(int, char *, struct OIDStruct *);
 int create_request(netsnmp_session *, struct OIDStruct **, char **, int,
 				   netsnmp_pdu **, unsigned int sleep_usecs);
+void create_pdu(int, char **, netsnmp_pdu **, struct OIDStruct **, int, long);
